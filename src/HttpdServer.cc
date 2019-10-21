92154c194947e557bf6a539b2378e26b37f9cf82
Qinyan Li <qy@Qinyans-MacBook-Pro.local>
2019-10-20 18:09:01 -0700

    modified sendfile

--- src/HttpdServer.cc
+++ src/HttpdServer.cc
@@ -275,7 +275,8 @@ int HttpdServer::handle_request(char *buf, int client_sock)
 		int fd = open(full_path.c_str(), O_RDONLY);
 		fstat(fd, &finfo);
 		off_t off = 0;
-		int h = sendfile(fd, client_sock, 0, &off, NULL, 0);
+		// int h = sendfile(fd, client_sock, 0, &off, NULL, 0);   // os version
+		int h = sendfile(client_sock, fd, 0, finfo.st_size);
 		log->info("sendfile status: {}", h);
 	}
 	return close;
